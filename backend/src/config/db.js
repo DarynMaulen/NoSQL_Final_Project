924c5296f6fa41d9eb9a32cf7f74125001912878
Дарын <241245@astanait.edu.kz>
2026-01-30 11:35:18 +0500

    separate express app from server listener for testing and added add seed script with Personal Blog content. fixes #14

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    const uri = process.env.MONGODB_URI;
-    if (!uri) throw new Error('MONGODB_URI not set in .env');
-
-    await mongoose.connect(uri)
-        .then(() => console.log('Connected to MongoDB Atlas'))
-        .catch((err) => console.error('Could not connect to MongoDB', err));
+const connectDB = async (uri = process.env.MONGODB_URI) => {
+    if (!uri) throw new Error('MONGODB_URI not set');
+    await mongoose.connect(uri, {});
+    console.log('MongoDB connected:', mongoose.connection.host || uri);
 };
 
 module.exports = connectDB;
